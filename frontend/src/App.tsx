bc02f3df55eeb2bdaa0af3feb8b7f87c0cb568f5
Jatin_Gorana <jatingorana123@gmail.com>
2025-03-28 16:20:47 +0530

    updated discover artifacts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Auth from './pages/Auth';
+import DiscoverArtifact from './pages/DiscoverArtifact';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
+        <Route path="/discover-artifacts" element={<DiscoverArtifact />} />
       </Routes>
     </>
   );
